8aee2bae5a8feb969a734e1320991013db5f95c5
Renée De Voursney <renee.devoursney@gmail.com>
2011-11-22 18:44:40 -0800

    add some code to test

---
+++ lib/timecapsule.rb
@@ -0,0 +1,49 @@
+class Timecapsule
+
+  config = YAML.load_file("#{RAILS_ROOT}/config/timecapsule.yml")
+  IMPORT_DIR = config[:import_directory] || 'db/seed_data/'
+  EXPORT_DIR = config[:export_directory] || 'db/seed_data/'
+  
+  require 'csv'
+  
+  def self.import_model(model_import)
+    puts "Importing: #{model_import} from #{IMPORT_DIR}#{model_import.to_s.pluralize.underscore}.csv"
+    csv = CSV.read(Rails.root.join("#{IMPORT_DIR}#{model_import.to_s.pluralize.underscore}.csv"))
+    attributes = csv.shift
+    csv.each do |row|
+      hash = {}
+      attributes.collect{|k| k.to_sym}.each_with_index do |key,i|
+        hash[key] = row[i]
+      end
+      object = model_import.new(hash)
+      object.save(false)
+    end
+  end
+  
+  def self.import
+    @csv_files = Dir.glob("#{IMPORT_DIR}*.csv")
+    @csv_files.each do |file|
+      model_name = file.split('/').last.split('.').first.classify.constantize
+      if model_name.count == 0
+        Timecapsule.import_model(model_name)
+      end
+    end
+  end
+  
+  def self.export_model(model_export)
+    puts "Exporting: #{model_export} to #{EXPORT_DIR}#{model_export.to_s.pluralize.underscore}.csv"
+    @file = File.open("#{EXPORT_DIR}#{model_export.to_s.pluralize.underscore}.csv", "w")
+    @file.puts model_export.first.attributes.collect{|k,v| k}.join(",")
+    model_export.all.each do |item|
+      @file.puts item.attributes.collect{|k,v| "#{Timecapsule.output(v)}"}.join(",")
+    end
+  end
+  
+  def self.output(value)
+    if value.is_a?(String)
+      return value.gsub(",",'')
+    end
+    value
+  end
+  
+end
--- test/test_timecapsule.rb
+++ test/test_timecapsule.rb
@@ -1,7 +1,16 @@
 require 'helper'
+require 'rails'
+
+class User < ActiveRecord::Base
+  attr_accessor :first_name, :last_name, :username, :password
+end
 
 class TestTimecapsule < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  should "export a model" do
+    flunk('FIGURE OUT HOW TO TEST THIS!!')
+    # u = User.new
+    # u.first_name = 'test'
+    # Timecapsule.export_model(User)
+    # assert_equal File.exists?("#{Timecapsule::EXPORT_DIR}#{User.to_s.pluralize.underscore}.csv"), true
   end
 end
